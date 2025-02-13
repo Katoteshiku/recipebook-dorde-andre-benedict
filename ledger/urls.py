7278d8021b021f4605267ca217b76f4ff809664f
Katoteshiku <andre11obrero@gmail.com>
2025-02-13 23:54:43 +0800

    Recipe templates made

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import recipe_list
+from .views import recipe_list, recipe_one, recipe_two
 
 urlpatterns = [
-    path('recipes/list', recipe_list, name='recipe-list')
+    path('recipes/list', recipe_list, name='recipe-list'),
+    path('recipe/1', recipe_one, name='recipe-one'),
+    path('recipe/2', recipe_two, name='recipe-two'),
 ]
 
 app_name = "ledger"
