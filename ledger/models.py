4981ebdf1bd969eb722c3bf3df925a246d51459b
Katoteshiku <andre11obrero@gmail.com>
2025-02-27 22:23:38 +0800

    Added functions to models

--- ledger/models.py
+++ ledger/models.py
@@ -1,14 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return str(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient', kwargs={'pk': self.pk})
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return str(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', kwargs={'pk': self.pk})
+
 
 class RecipeIngredient(models.Model):
     quantity = models.PositiveIntegerField()
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.SET_NULL,
+        related_name="recipe"
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        related_name="ingredients"
+    )
