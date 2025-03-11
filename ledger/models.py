47eb68e31eaa107ce2c0a6f46135d934656df7c3
Katoteshiku <andre11obrero@gmail.com>
2025-03-12 01:06:58 +0800

    fixed PEP8 errors

--- ledger/models.py
+++ ledger/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
