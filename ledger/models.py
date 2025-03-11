ef4ed0a0f6c57c5a8b0ebe89e4b11f15a03033ee
Katoteshiku <andre11obrero@gmail.com>
2025-03-11 23:12:07 +0800

    Made Profile model and edited Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -12,8 +13,31 @@ class Ingredient(models.Model):
         return reverse('ledger:ingredient', kwargs={'pk': self.pk})
 
 
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255)
+        ]
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
