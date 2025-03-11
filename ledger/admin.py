df0074bb2b1357948315aa810b1177d8de8ae293
Katoteshiku <andre11obrero@gmail.com>
2025-03-12 00:32:26 +0800

    Login Flow has been made

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from .models import Recipe, RecipeIngredient, Ingredient, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class Admin(UserAdmin):
+    inlines = [
+        ProfileInLine,
+    ]
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -19,3 +32,5 @@ class IngredientAdmin(admin.ModelAdmin):
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, Admin)
